c98561798f17d12b624b5ab5c246751cf103dd20
admin <SSaccounting@DO-11.local>
2019-08-28 17:10:27 -0700

    take2

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://kallistahalim:Moderndance123@k9tinder-bjtam.mongodb.net/test?retryWrites=true&w=majority',
+    secretOrKey: 'secret'     
+};
+
+//The text above is the db connection string pulled from Mongo DB Atlas (previous mLab). 
